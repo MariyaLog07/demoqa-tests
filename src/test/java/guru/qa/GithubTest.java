577125af30d04f6cc4dffd414267b0f8a6a0229f
vladi <vladimirblokhin@yandex.ru>
2021-09-12 17:57:37 +0300

    Added autotest for finding a JUnit5 sample code

---
+++ src/test/java/guru/qa/GithubTest.java
@@ -0,0 +1,27 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubTest {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void shouldFindJUnit5SampleCode() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $(".wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+    }
+}
