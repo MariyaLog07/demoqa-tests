0859cbc7368e3b53fbd89b7ce1feb1c3451af77d
vladi <vladimirblokhin@yandex.ru>
2021-10-19 20:04:03 +0300

    add properties

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -2,21 +2,29 @@ package guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import guru.qa.config.CredentialsConfig;
 import guru.qa.tests.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.aeonbits.owner.ConfigFactory;
+import static java.lang.String.format;
 
 
 public class TestBase {
+    static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+
+    static String loginForSelenoid = credentials.login();
+    static String passwordForSelenoid = credentials.password();
 
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.startMaximized = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote =
+                format("https://%s:%s@%s", loginForSelenoid, passwordForSelenoid, System.getProperty("remoteUrl"));
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
